1cad47c638ac2b22a17031a19fa9646d0da3e4b1
Khushbu1309 <khushbubora711@gmail.com>
2023-03-19 01:28:00 +0530

    @Controller annotation indicates that a particular class serves the role of a controller.

---
+++ SpringWebProject/src/main/java/com/hatisha/demo/HomeController.java
@@ -0,0 +1,19 @@
+
+package com.hatisha.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("home")
+//	@ResponseBody
+	public String home(){
+		System.out.println("hii");
+		return "home.jsp";
+		
+	}
+
+}
