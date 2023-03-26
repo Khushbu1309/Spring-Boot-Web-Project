2a8132cbc505ae0a73375ff18dba8cd42b14585d
Khushbu1309 <khushbubora711@gmail.com>
2023-03-27 00:29:46 +0530

    Fetch request from client without HttpRequest

--- SpringWebProject/src/main/java/com/hatisha/demo/HomeController.java
+++ SpringWebProject/src/main/java/com/hatisha/demo/HomeController.java
@@ -3,6 +3,7 @@ package com.hatisha.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,9 +14,9 @@ public class HomeController {
 	
 	@RequestMapping("home")
 //	@ResponseBody
-	public String home(HttpServletRequest req){
-		HttpSession session=req.getSession();
-		String name=req.getParameter("name");
+	public String home(@RequestParam("myname") String name,HttpSession session ){
+//		HttpSession session=req.getSession();
+//		String name=req.getParameter("name");
 		
 		System.out.println("hii "+name);
 		session.setAttribute("name", name);
